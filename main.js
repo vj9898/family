673b75be0f6b53f3eab2742b95f920a27e7217d8
vj9898 <81862570+vj9898@users.noreply.github.com>
2021-04-05 16:48:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var images =[
+    "file:///C:/Users/HP/OneDrive/Desktop/family/91zbi9M+mKL.jpg",
+    "file:///C:/Users/HP/OneDrive/Desktop/family/download.png",
+    "file:///C:/Users/HP/OneDrive/Desktop/family/mother-drawing-child-clip-art-png-favpng-q8cDP2Q3QZR622b4Tc1gShhFn.jpg",
+    "file:///C:/Users/HP/OneDrive/Desktop/family/f0caf9ad93bfea30a46faa27e6ac56ba.jpg",
+    "file:///C:/Users/HP/OneDrive/Desktop/family/images.png",
+  ];                           
+  var i=1; function nextimage() { document.getElementById("monkey").src = images[i]; i++; }
+  if(i==5){
+    i=1;
+  }
